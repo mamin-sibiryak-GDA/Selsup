313ab87eb772b5866aa5f40c1d4d2d5957c1f2b5
mamin-sibiryak-GDA <mincrafterdemo300@gmail.com>
2025-08-08 16:30:41 +0300

    Initial commit

---
+++ src/main/java/com/example/selsup/SelsupApplication.java
@@ -0,0 +1,13 @@
+package com.example.selsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelsupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelsupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/selsup/SelsupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelsupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
